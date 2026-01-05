c5b10fed1af8c0a5181a76f517c33511074d740a
Hraban <hraban@0brg.net>
2026-01-05 17:07:22 -0500

    build: make script available as package on flake

--- flake.nix
+++ flake.nix
@@ -15,14 +15,22 @@
     flake-parts.lib.mkFlake { inherit inputs; } ({
       systems = import inputs.systems;
       imports = [ inputs.treefmt-nix.flakeModule ];
-      perSystem = {
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs.nixfmt = {
-            enable = true;
-            strict = true;
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt = {
+              enable = true;
+              strict = true;
+            };
           };
+          # This derivation needs git and grep on the PATH.  Those dependencies
+          # could be baked in from this flake’s nixpkgs, but that feels like more
+          # trouble than it’s worth: it’s highly unlikely this will ever be used
+          # in a system without grep, let alone git (!), and it’s nicer to always
+          # default to the system tools.
+          packages.default = pkgs.writeShellScriptBin "nocommit-pre-commit" (builtins.readFile ./pre-commit);
         };
-      };
     });
 }
